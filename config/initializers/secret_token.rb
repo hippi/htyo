f8ad595dfab6abb7b50dee0659ecd2f11d75ac24
jee <spamloota@gmail.com>
2012-10-16 09:15:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harkkatyo::Application.config.secret_token = '4a21151c48037a55c531f5451df9f8a89503cded7bb5d2b3cb9bf68bffe3107493193e922ddaea6710603771bf51aec3b9668384d19826e9bd0490f1144c4a01'
